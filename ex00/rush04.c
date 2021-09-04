e3570c529f4b44ba6226f9e393c14a904e622987
Theo <tbrulhar@student.42lausanne.ch>
2021-09-04 19:44:42 +0200

    ex03 et ex04 normine, good to go

--- ex00/rush04.c
+++ ex00/rush04.c
@@ -6,19 +6,23 @@
 /*   By: the TAP						            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/04 11:12:51 by TAP		       #+#    #+#             */
-/*   Updated: 2021/09/04 17:42:07 by tbrulhar         ###   ########.fr       */
+/*   Updated: 2021/09/04 18:32:04 by tbrulhar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 char	ft_putchar(char c);
 
-void	ft_first_last_colum(int icol, int columns)
-{
-	if (icol == 0)
+void	ft_1_last_line(int irow, int rows, int icol, int col)
+{			
+	if (icol == 0 && irow == 0)
 	{
 		ft_putchar('A');
 	}
-	else if (icol == columns - 1)
+	else if (icol == col - 1 && irow == rows - 1)
+	{
+		ft_putchar('A');
+	}
+	else if ((icol == 0 && irow == rows - 1) || (icol == col - 1 && irow == 0))
 	{
 		ft_putchar('C');
 	}
@@ -32,7 +36,7 @@ void	ft_columns(int irow, int rows, int icol, int columns)
 {
 	if (irow == 0 || irow == rows - 1)
 	{
-		ft_first_last_colum(icol, columns);
+		ft_1_last_line (irow, rows, icol, columns);
 	}
 	else
 	{
