4bb9d70b5873c7a802ff5183f8c0ba3a56c25627
Aurelien Borst <aborst@student.42lausanne.ch>
2021-09-06 21:18:59 +0200

    ex04

---
+++ c01/ex04/ft_ultimate_div_mod.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_div_mod.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aborst <aborst@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/06 16:37:24 by aborst            #+#    #+#             */
+/*   Updated: 2021/09/06 21:18:26 by aborst           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+void	ft_ultimate_div_mod(int *a, int *b)
+{
+	int	temp;
+
+	temp = *a;
+	*a = *a / *b;
+	*b = temp % *b;
+}
+
+/* int	main(void)
+{
+	int	*a;
+	int	*b;
+
+	a = 700;
+	b = 7;
+	printf("a = %d\n", a);
+	printf("b = %d\n", b);
+	ft_ultimate_div_mod(&a, &b);
+	printf("a = %d\n", a);
+	printf("b = %d\n", b);
+} */
