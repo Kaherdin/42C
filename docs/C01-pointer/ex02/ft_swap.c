8a2e1c64780bd350883b4c05ec7dae396f0e20a3
Aurelien Borst <aborst@student.42lausanne.ch>
2021-09-06 19:52:15 +0200

    cleaning ex02

--- docs/C01-pointer/ex02/ft_swap.c
+++ docs/C01-pointer/ex02/ft_swap.c
@@ -6,7 +6,7 @@
 /*   By: aborst <aborst@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/06 16:37:24 by aborst            #+#    #+#             */
-/*   Updated: 2021/09/06 19:51:58 by aborst           ###   ########.fr       */
+/*   Updated: 2021/09/06 19:50:39 by aborst           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,16 +21,24 @@ void	ft_swap(int *a, int *b)
 	*b = temp_a;
 }
 
-/* int	main(void)
+int	main(void)
 {
 	int	*pb;
 	int	*pa;
+	int	*ptra1;
+	int	*ptrb1;
 
 	pa = 13;
 	pb = 23;
+	ptra1 = &pa;
+	ptrb1 = &pb;
+	printf("ptra1 = %d\n", *ptra1);
+	printf("ptrb1 = %d\n", *ptrb1);
 	printf("a = %d\n", pa);
 	printf("b = %d\n", pb);
 	ft_swap(&pa, &pb);
+	printf("ptraSwap = %d\n", *ptra1);
+	printf("ptrbSwap = %d\n", *ptrb1);
 	printf("aSwap = %d\n", pa);
 	printf("bSwap = %d\n", pb);
-} */
+}
