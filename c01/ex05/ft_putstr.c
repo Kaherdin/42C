15cf1b085681b05def6a5c47cdc81fd1b6e44a58
Aurelien Borst <aborst@student.42lausanne.ch>
2021-09-06 16:39:15 +0200

    ex00 et ex05 : done

--- c01/ex05/ft_putstr.c
+++ c01/ex05/ft_putstr.c
@@ -1,30 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_ft.c                                            :+:      :+:    :+:   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aborst <aborst@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/09/06 16:38:15 by aborst            #+#    #+#             */
-/*   Updated: 2021/09/06 16:38:43 by aborst           ###   ########.fr       */
+/*   Created: 2021/09/06 16:37:24 by aborst            #+#    #+#             */
+/*   Updated: 2021/09/06 16:38:34 by aborst           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
-void	ft_ft(int *nbr_point)
+void	ft_putstr(char *str)
 {
-	*nbr_point = 42;
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		printf("%c", str[i]);
+		i++;
+	}
 }
 
 /* int	main(void)
 {
-	int	number;
-	int	*ptr;
-
-	number = 20;
-	printf("%d\n", number);
-	ptr = &number;
-	ft_ft(ptr);
-	printf("%d\n", number);
+	ft_putstr("1111010101010");
 } */
