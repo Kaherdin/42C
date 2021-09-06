2014b992b17a2ce8c9df2e0b40cdc5639bb3987f
Aurelien Borst <aborst@student.42lausanne.ch>
2021-09-06 20:35:47 +0200

    ex03

--- docs/C01-pointer/ex03/ft_div_mod.c
+++ docs/C01-pointer/ex03/ft_div_mod.c
@@ -1,36 +1,50 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_swap.c                                          :+:      :+:    :+:   */
+/*   ft_div_mod.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aborst <aborst@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/06 16:37:24 by aborst            #+#    #+#             */
-/*   Updated: 2021/09/06 19:55:10 by aborst           ###   ########.fr       */
+/*   Updated: 2021/09/06 20:33:27 by aborst           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include <stdio.h>
+#include <stdio.h>
 
-void	ft_swap(int *a, int *b)
+void	ft_div_mod(int a, int b, int *div, int *mod)
 {
-	int	temp_a;
-
-	temp_a = *a;
-	*a = *b;
-	*b = temp_a;
+	*div = a / b;
+	*mod = a % b;
 }
 
 /* int	main(void)
 {
-	int	*pb;
-	int	*pa;
+	int	b;
+	int	a;
+	int	div;
+	int	mod;
 
-	pa = 13;
-	pb = 23;
-	printf("a = %d\n", pa);
-	printf("b = %d\n", pb);
-	ft_swap(&pa, &pb);
-	printf("aSwap = %d\n", pa);
-	printf("bSwap = %d\n", pb);
+	a = 50;
+	b = 7;
+	printf("div = %d\n", div);
+	ft_div_mod(a, b, &div, &mod);
+	printf("div = %d\n", div);
+	printf("mod = %d\n", mod);
 } */
+
+int	main(void)
+{
+	int	b;
+	int	a;
+	int	*div;
+	int	*mod;
+
+	a = 50;
+	b = 7;
+	printf("div = %d\n", div);
+	printf("mod = %d\n", mod);
+	ft_div_mod(a, b, &div, &mod);
+	printf("div = %d\n", div);
+	printf("mod = %d\n", mod);
+}
