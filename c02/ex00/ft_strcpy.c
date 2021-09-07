5853c6211fcd113196bd8ca2d5373dc36ba481a9
Aurelien Borst <aborst@student.42lausanne.ch>
2021-09-07 18:48:22 +0200

    c02 : ex00 + docs

---
+++ c02/ex00/ft_strcpy.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+char	*ft_strcpy(char *dest, char *src)
+{
+	int	i;
+
+	i = 0;
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = src[i];
+	return (dest);
+}
+
+/* int	main(void)
+{
+	char	*dest;
+	char	*src;
+
+	dest = "Tu vas rater";
+	src = "Tu vas réussir !";
+	printf("String 1 = %s\n", dest);
+	printf("String 2 = %s\n", src);
+	ft_strcpy(&dest, &src);
+	printf("String 1 is now = %s\n", dest);
+	return (0);
+}
+ */
---
+++ docs/C02-more-functions/ex00/ft_strcpy_42.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+char	*ft_strcpy(char *dest, char *src)
+{
+	int	i;
+
+	i = 0;
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = src[i];
+	return (dest);
+}
+
+int	main(void)
+{
+	//La taille des 2 strings est differentes, du coup, il y a une erreur
+	char dest[] = "Tu vas rater !";
+	char src[] = "Tu vas réussir !";
+
+	printf("String 1 = %s\n", dest);
+	printf("String 2 = %s\n", src);
+	ft_strcpy(dest, src);
+	//printf("String 1 is now = %s\n", &dest);
+	return (0);
+}
+
+/* int	main(void)
+{
+	char	*dest;
+	char	*src;
+
+	dest = "Tu vas rater";
+	src = "Tu vas réussir !";
+	printf("String 1 = %s\n", dest);
+	printf("String 2 = %s\n", src);
+	ft_strcpy(&dest, &src);
+	printf("String 1 is now = %s\n", dest);
+	return (0);
+}
+ */
