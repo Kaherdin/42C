6f423ee8d9dec646d74209350b900bcd58a97e3a
Aurelien Borst <aborst@student.42lausanne.ch>
2021-09-08 01:00:47 +0200

    tested & OK wo main - ex05 + (ex00, ex01, ex02, ex03, ex04)

--- c02/ex05/ft_str_is_uppercase.c
+++ c02/ex05/ft_str_is_uppercase.c
@@ -1,33 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_str_is_lowercase.c                              :+:      :+:    :+:   */
+/*   ft_str_is_uppercase.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aborst <aborst@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/07 18:55:36 by aborst            #+#    #+#             */
-/*   Updated: 2021/09/08 01:02:54 by aborst           ###   ########.fr       */
+/*   Updated: 2021/09/08 01:02:42 by aborst           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
-int	cond_check_lowercase(char c)
+int	cond_check_uppercase(char c)
 {
-	if ((c >= 'a' && c <= 'z'))
+	if ((c >= 'A' && c <= 'Z'))
 		return (1);
 	else
 		return (0);
 }
 
-int	ft_str_is_lowercase(char *str)
+int	ft_str_is_uppercase(char *str)
 {
 	int	i;
 
 	i = 0;
 	while (str[i] != '\0')
 	{
-		if (!cond_check_lowercase(str[i]))
+		if (!cond_check_uppercase(str[i]))
 		{
 			return (0);
 		}			
@@ -38,6 +38,6 @@ int	ft_str_is_lowercase(char *str)
 
 /* int	main(void)
 {	
-	printf("%d\n", ft_str_is_lowercase("asssasas"));
+	printf("%d\n", ft_str_is_uppercase("a"));
 	return (0);
 } */
