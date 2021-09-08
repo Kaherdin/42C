28b60f0ff6975df99b64e13087b560b39dd315fe
Aurelien Borst <aborst@student.42lausanne.ch>
2021-09-08 03:18:03 +0200

    Tested w/o main : ex08

---
+++ c02/ex08/ft_strlowcase.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlowcase.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aborst <aborst@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/07 18:55:36 by aborst            #+#    #+#             */
+/*   Updated: 2021/09/08 03:17:30 by aborst           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+char	*ft_strlowcase(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] >= 'A' && str[i] <= 'Z')
+		{
+			str[i] = str[i] + 32;
+		}		
+		i++;
+	}
+	return (str);
+}
+
+/* int	main(void)
+{	
+	char	str1[] = "AuReLieN";
+	printf("Minuscle : %s\n", str1);
+	ft_strlowcase(str1);
+	printf("Majuscule : %s\n", str1); 
+}
+ */
