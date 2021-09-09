ef4d6bf3d3cb63078d76c8291adf342d1da12c48
Aurelien Borst <aborst@student.42lausanne.ch>
2021-09-09 22:53:34 +0200

    C05, ex01 : ok

---
+++ c05/ft_iterative_factorial.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_iterative_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aborst <aborst@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/09 22:47:22 by aborst            #+#    #+#             */
+/*   Updated: 2021/09/09 22:53:21 by aborst           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <unistd.h>
+
+int	ft_iterative_factorial(int nb)
+{
+	int	i;
+
+	if (nb < 1)
+	{
+		return (0);
+	}		
+	i = nb;
+	while (i > 1)
+	{
+		nb = nb * (i - 1);
+		i--;
+	}
+	return (nb);
+}
+
+/* int	main(void)
+{
+	int	result;
+
+	result = ft_iterative_factorial(5); // 5*4*3*2*1 = 120
+	printf("%d", result);
+} */
