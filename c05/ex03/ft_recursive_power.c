2bc586ed46ece94424b57bfe0fdefafceb6c5d12
Aurelien Borst <aborst@student.42lausanne.ch>
2021-09-10 00:19:05 +0200

    C05, ex03 : ok

---
+++ c05/ex03/ft_recursive_power.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_power.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aborst <aborst@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/09 22:47:22 by aborst            #+#    #+#             */
+/*   Updated: 2021/09/10 00:18:48 by aborst           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <unistd.h>
+
+int	ft_recursive_power(int nb, int power)
+{
+	if (power == 0)
+	{
+		return (1);
+	}
+	else if (power < 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (nb * ft_recursive_power(nb, power - 1));
+	}
+}
+
+/* int	main(void)
+{
+	int	result;
+
+	result = ft_recursive_power(5, 4); // 5*5*5*5 = 625
+	printf("%d", result);
+} */
