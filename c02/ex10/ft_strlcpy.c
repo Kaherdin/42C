5996bb61c224fae7ef75925cf01ae563e21e7abd
Aurelien Borst <aborst@student.42lausanne.ch>
2021-09-14 16:23:16 +0200

    ec02/ex10 try

--- c02/ex10/ft_strlcpy.c
+++ c02/ex10/ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: aborst <aborst@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/07 18:55:36 by aborst            #+#    #+#             */
-/*   Updated: 2021/09/14 14:20:10 by aborst           ###   ########.fr       */
+/*   Updated: 2021/09/14 16:13:21 by aborst           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,17 +28,38 @@ unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
 	return (i);
 }
 
-/* int	main(void)
+int	main(void)
 {
-	char	src1[50]	= "abcdefghi";
-	char	dest1[50]   = "asdasd";
-	unsigned int 	nb2 = 8;
+ 	//char	src1[256]	= "abcdefghi";
+	//char	dest1[50]   = "asdasd";
+	//unsigned int 	nb2 = 8;
 
 	//char	src[50]	= "ab121";
 	//char	dest[50]	= "12123123"; 
 	//size_t 			nb = 8;
 
-	printf("Original strlcpy : %ld \n", strlcpy(dest1, src1, nb2));
-	printf("Copy ft_strlcpy: %u", ft_strlcpy(dest1, src1, nb2));
+	//printf("Original strlcpy : %ld \n", strlcpy(dest1, src1, nb2));
+	//printf("Copy ft_strlcpy: %u", ft_strlcpy(dest1, src1, nb2));
+
+	char test[256] = "\0zxcvzxcvzxcvxzcvzxcv";
+	printf("%d-", ft_strlcpy(test, "asdf", 16));
+	printf("%s\n", test);
+	printf("%d-", ft_strlcpy(test, "uiop", 0));
+	printf("%s\n", test);
+	printf("%d-", ft_strlcpy(test, "qwerty", 4));
+	printf("%s\n", test);
+	printf("%d-", ft_strlcpy(test, "", 4));
+	printf("%s\n", test);
+
+	printf("----------- \n");
+
+ 	printf("%d-", strlcpy(test, "asdf", 16));
+	printf("%s\n", test);
+	printf("%d-", strlcpy(test, "uiop", 0));
+	printf("%s\n", test);
+	printf("%d-", strlcpy(test, "qwerty", 4));
+	printf("%s\n", test);
+	printf("%d-", strlcpy(test, "", 4));
+	printf("%s\n", test);
 	return (0);
-} */
+}
