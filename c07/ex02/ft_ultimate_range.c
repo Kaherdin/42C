14b3121dca426cf9d5b7467e8a94ad9a416a23d1
Aurelien Borst <aborst@student.42lausanne.ch>
2021-09-14 19:57:35 +0200

    c07/ex02 : test ok

---
+++ c07/ex02/ft_ultimate_range.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_range.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aborst <aborst@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/14 19:33:51 by aborst            #+#    #+#             */
+/*   Updated: 2021/09/14 19:57:04 by aborst           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <inttypes.h>
+
+int	ft_ultimate_range(int **range, int min, int max)
+{
+	int	i;
+	int	*tab;
+
+	if (min >= max)
+	{
+		*range = (void *)0;
+		return (0);
+	}
+	tab = (int *)malloc(sizeof(*tab) * (max - min));
+	i = 0;
+	while (min < max)
+	{
+		tab[i] = min;
+		i++;
+		min++;
+	}
+	*range = tab;
+	return (i);
+}
+
+/* int	main(void)
+{
+	int	*res = NULL;
+	int	i;
+	int	size;
+
+	size = ft_ultimate_range(&res, 5, 10);
+	printf("is_null? %d\n", res == NULL);
+	printf("size is %i\n", size);
+	for (i = 0; i < 5; i++)
+		printf("%d,", res[i]);
+	printf("\n");
+
+	res = NULL;
+	size = ft_ultimate_range(&res, -20, -17);
+	printf("is_null? %d\n", res == NULL);
+	printf("size is %i\n", size);
+	for (i = 0; i < 3; i++)
+		printf("%d,", res[i]);
+	printf("\n");
+
+	res = (int*)1;
+	size = ft_ultimate_range(&res, 10, 5);
+	printf("is_null? %d\n", res == NULL);
+	printf("size is %i\n", size);
+} */
