2aee66bbc2d43287b40738c5c6f413a15915c09b
Aurelien Borst <aborst@student.42lausanne.ch>
2021-09-16 17:31:37 +0200

    C05/ex00-01 : better example

--- c05/ex01/ft_recursive_factorial.c
+++ c05/ex01/ft_recursive_factorial.c
@@ -6,7 +6,7 @@
 /*   By: aborst <aborst@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/09 22:47:22 by aborst            #+#    #+#             */
-/*   Updated: 2021/09/10 00:11:41 by aborst           ###   ########.fr       */
+/*   Updated: 2021/09/16 17:31:19 by aborst           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,9 +31,25 @@ int	ft_recursive_factorial(int nb)
 
 /* int	main(void)
 {
-	int	result;
+	int	result, expected;
 
-	result = ft_recursive_factorial(5); // 5*4*3*2*1 = 120
-	printf("%d", result);
-}
- */
+	result = ft_recursive_factorial(13), expected = 1932053504;
+	printf("13! (%d vs %d) -> %d\n", result, expected, result == expected);
+
+ 	result = ft_recursive_factorial(4), expected = 24; //4*3*2*1 = 24
+	printf("4! (%d vs %d) -> %d\n", result, expected, result == expected);
+	result = ft_recursive_factorial(0), expected = 1;
+	printf("0! (%d vs %d) -> %d\n", result, expected, result == expected);
+	result = ft_recursive_factorial(1), expected = 1;
+	printf("1! (%d vs %d) -> %d\n", result, expected, result == expected);
+	result = ft_recursive_factorial(10), expected = 3628800;
+	printf("10! (%d vs %d) -> %d\n", result, expected, result == expected);
+	result = ft_recursive_factorial(12), expected = 479001600;
+	printf("12! (%d vs %d) -> %d\n", result, expected, result == expected);
+	result = ft_recursive_factorial(13), expected = 1932053504;
+	printf("13! (%d vs %d) -> %d\n", result, expected, result == expected);
+	result = ft_recursive_factorial(-2), expected = 0;
+	printf("-2! (%d vs %d) -> %d\n", result, expected, result == expected);
+	result = ft_recursive_factorial(1000), expected = 0;
+	printf("1000! (%d vs %d) -> %d\n", result, expected, result == expected);
+} */
