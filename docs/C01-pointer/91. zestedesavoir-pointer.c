2e06cd51c99a679606493aaf76425724c9aba7fc
Aurelien Borst <aborst@student.42lausanne.ch>
2021-09-17 15:06:13 +0200

    exam inter play

--- docs/C01-pointer/91. zestedesavoir-pointer.c
+++ docs/C01-pointer/91. zestedesavoir-pointer.c
@@ -14,15 +14,18 @@ int main(void)
     int b;
     int *pa = &a;
     int *pb = &b;
-    int *pc = malloc(sizeof(int) * 10);
-    printf("%p", pc);
+    int pc = 222;
+    int pd = 333;
+    //int *pc = malloc(sizeof(int) * 10);
+    //printf("%p", pc);
 
     //str = (char *) malloc(sizeof(*str) * (len + 1));
 
     test(&a, &b); //Idem
     test(pa, pb); //Idem
-    //printf("a = %d, b = %d\n", a, b);
-    //printf("a = %d, b = %d\n", *pa, *pb);
+    printf("a = %d, b = %d\n", a, b);
+    printf("a = %d, b = %d\n", *pa, *pb);
+    printf("pc = %d, od = %d\n", pc, pd);
     return 0;
 
     
---
+++ exam/inter.c
@@ -0,0 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   inter.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aborst <aborst@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/17 14:07:53 by aborst            #+#    #+#             */
+/*   Updated: 2021/09/17 14:52:59 by aborst           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+int	ft_check(char *str, char c, int size)
+{	
+	int	i;
+
+	i = 0;
+	while (i < size)
+	{
+		if (c == str[i])
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+void	ft_inter(char *s1, char *s2)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (s1[i])
+	{
+		if (ft_check(s1, s1[i], i) == 1)
+		{
+			j = 0;
+			while (s2[j])
+			{
+				if (s2[j] == s1[i])
+				{
+					ft_putchar(s1[i]);
+					break;
+				}
+				++j;
+			}
+		}			
+		++i;
+	}
+/* 	while (str2[s2_i])
+	{
+		if (ft_check_double(str1, str2[s2_i], s1_i) == 1)
+		{
+			if (ft_check_double(str2, str2[s2_i], s2_i) == 1)
+				ft_putchar(str2[s2_i]);
+		}		
+		s2_i++;
+	} */
+}
+
+int	main(void)
+{
+	ft_inter("padinton", "paqefwtdjetyiytjneytjoeyjnejeyj");
+}
