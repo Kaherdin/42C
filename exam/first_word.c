e789cd95812b8a6c9a5e109567fa7a12ee274d5e
Aurelien Borst <aborst@student.42lausanne.ch>
2021-09-17 02:56:34 +0200

    examtrain : first_word.c

---
+++ exam/first_word.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   first_word.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aborst <aborst@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/17 02:40:39 by aborst            #+#    #+#             */
+/*   Updated: 2021/09/17 02:56:18 by aborst           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_first_word(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n')
+		i++;
+	while (str[i] != ' ' && str[i] != '\t' && str[i] != '\n' && str[i] != '\0')
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+//int	main(int argc, char **argv)
+int	main(void)
+{
+	ft_first_word("tesst asdsadas");
+}
