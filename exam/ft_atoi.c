3798c530863c9a78cb1dfd5469c1a432d5c49308
Kaherdin <aurelien.borst@gmail.com>
2021-09-19 14:35:48 +0200

    Exam : good example of atoi with 2 func

---
+++ exam/ft_atoi.c
@@ -0,0 +1,37 @@
+
+int	ft_whitespace(char c)
+{
+	if(c = ' ' || c = '\n' || c = '\v' || c = '\t' || c = '\f' || c = '\r' || )
+		return (1);
+	else
+		return (0);
+}
+
+bool	ft_is_number(char c)
+{
+	return (c >= '0' && c <= '9');
+}
+
+int		ft_atoi(const char *str)
+{
+	int	result;
+	int	minus;
+
+	minus = 1;
+	while (ft_is_whitespace(*str))
+		str++;
+	if (*str == '-' || *str == '+')
+	{
+		if (*str == '-')
+			minus *= -1;
+		str++;
+	}
+	result = 0;
+	while (ft_is_number(*str))
+	{
+		result *= 10;
+		result += *str - '0';
+		str++;
+	}
+	return (result * minus);
+}
