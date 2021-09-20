ac195168ea987548ee747644077196e8d66279a3
Aurelien Borst <aborst@student.42lausanne.ch>
2021-09-20 23:08:03 +0200

    C07/ex03 : strjoin good

---
+++ c07/ex03/ft_strjoin.c
@@ -0,0 +1,86 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aborst <aborst@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/20 19:17:20 by aborst            #+#    #+#             */
+/*   Updated: 2021/09/20 23:07:45 by aborst           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int	ft_strlen(char *str)
+{
+	int	len;
+
+	len = 0;
+	while (str[len])
+		len++;
+	return (len);
+}
+
+int	ft_strslen(char **strs, int size, char *sep)
+{
+	int	i;
+	int	lens;
+
+	i = 0;
+	lens = 0;
+	while (i < size)
+	{
+		lens = lens + ft_strlen(strs[i]);
+		i++;
+	}
+	lens = lens + (size - 1) * ft_strlen(sep) + 1;
+	return (lens);
+}
+
+char	*ft_strcat(char *dest, char *src)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (dest[i])
+		i++;
+	j = 0;
+	while (src[j])
+		dest[i + j] = src[j], j++;
+	dest[i + j] = '\0';
+	return (dest);
+}
+
+char	*ft_strjoin(int size, char **strs, char *sep)
+{
+	int		i;
+	char	*str;
+
+	if (size == 0)
+	{
+		str = malloc(sizeof(char));
+		*str = 0;
+		return (str);
+	}
+	str = malloc(sizeof(char) * ft_strslen(strs, size, sep));
+	if (!(str))
+		return (NULL);
+	*str = 0;
+	i = 0;
+	while (i < size)
+	{
+		ft_strcat(str, strs[i]);
+		if (i < size - 1)
+			ft_strcat(str, sep);
+		i++;
+	}
+	return (str);
+}
+
+int	main(int argc, char **argv)
+{
+	printf("%s\n", ft_strjoin(3, argv, " | "));
+}
