1960287c6ba402d9348288251a50f68a299920d8
Aurelien Borst <aborst@student.42lausanne.ch>
2021-09-20 16:11:49 +0200

    C06/ex03 : works but to be improve

---
+++ c06/ex03/ft_sort_params.c
@@ -0,0 +1,95 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aborst <aborst@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/14 03:18:56 by aborst            #+#    #+#             */
+/*   Updated: 2021/09/20 16:10:44 by aborst           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] == s2[i] && s1[i] && s2[i])
+	{
+		i++;
+	}
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+}
+
+void	ft_strswap(char **a, char **b)
+{
+	char	*c;
+
+	c = *a;
+	*a = *b;
+	*b = c;
+}
+
+/* int	main(int argc, char **argv)
+{
+	int	i;
+
+	i = argc - 1;
+	while (i > 0)
+	{
+		ft_putstr(argv[i]);
+		ft_putchar('\n');
+		i--;
+	}
+	return (0);
+} */
+
+int	main(int argc, char **argv)
+{
+	int		find;
+	int		i;
+
+	find = 1;
+	while (find)
+	{
+		find = 0;
+		i = 0;
+		while (++i < argc - 1)
+		{
+			if (ft_strcmp(argv[i], argv[i + 1]) > 0)
+			{
+				ft_strswap(&argv[i], &argv[i + 1]);
+				find = 1;
+			}
+		}
+	}
+	i = 1;
+	while (i < argc)
+	{
+		ft_putstr(argv[i]);
+		ft_putchar('\n');
+		i++;
+	}	
+	return (0);
+}
