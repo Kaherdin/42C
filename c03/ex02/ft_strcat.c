830f2f27cdafbaed9070500bdc44dbd20da00877
Aurelien Borst <aborst@student.42lausanne.ch>
2021-09-20 17:55:49 +0200

    c03/ex02 : cleaner

--- c03/ex02/ft_strcat.c
+++ c03/ex02/ft_strcat.c
@@ -6,7 +6,7 @@
 /*   By: aborst <aborst@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/07 18:55:36 by aborst            #+#    #+#             */
-/*   Updated: 2021/09/09 20:45:31 by aborst           ###   ########.fr       */
+/*   Updated: 2021/09/20 17:55:41 by aborst           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,20 +16,20 @@
 char	*ft_strcat(char *dest, char *src)
 {
 	int	i;
-	int	i2;
+	int	j;
 
 	i = 0;
-	while (dest[i] != '\0')
+	while (dest[i])
 	{
 		i++;
 	}		
-	i2 = 0;
-	while (src[i2] != '\0')
+	j = 0;
+	while (src[j])
 	{
-		dest[i + i2] = src[i2];
-		i2++;
+		dest[i + j] = src[j];
+		j++;
 	}
-	dest[i + i2] = '\0';
+	dest[i + j] = '\0';
 	return (dest);
 }
 
