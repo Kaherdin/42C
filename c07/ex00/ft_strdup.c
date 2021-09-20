a8a2739db1f50b49dd1da75fc680bc5f6b7efccb
Aurelien Borst <aborst@student.42lausanne.ch>
2021-09-20 16:54:31 +0200

    C07/ex00 et ex01, propre

--- c07/ex00/ft_strdup.c
+++ c07/ex00/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: aborst <aborst@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/14 19:33:51 by aborst            #+#    #+#             */
-/*   Updated: 2021/09/14 19:40:50 by aborst           ###   ########.fr       */
+/*   Updated: 2021/09/20 16:43:45 by aborst           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,26 @@
 #include <string.h>
 #include <stdio.h>
 
+int	ft_strlen(char *str)
+{
+	int	len;
+
+	len = 0;
+	while (str[len])
+	{
+		len++;
+	}
+	return (len);
+}
+
 char	*ft_strdup(char *src)
 {
 	int		i;
 	int		len;
 	char	*str;
 
-	len = 0;
-	while (src[len])
-		len++;
-	str = (char *) malloc(sizeof(*str) * (len + 1));
+	len = ft_strlen(src);
+	str = malloc(sizeof(*str) * (len + 1));
 	i = 0;
 	while (i < len)
 	{
@@ -42,7 +52,7 @@ char	*ft_strdup(char *src)
 	printf("%d, '%s' vs '%s'\n", res != str, res, str);
 	str = "", res = strdup(str);
 	printf("%d, '%s' vs '%s'\n", res != str, res, str);	
-	str = "hello world!", res = strdup(str);
+	str = "hello wor ld!", res = strdup(str);
 	printf("%d, '%s' vs '%s'\n", res != str, res, str);
 
 	printf("\n--------------\n\n");
@@ -51,7 +61,6 @@ char	*ft_strdup(char *src)
 	printf("%d, '%s' vs '%s'\n", res != str, res, str);
 	str = "", res = ft_strdup(str);
 	printf("%d, '%s' vs '%s'\n", res != str, res, str);	
-	str = "hello world!", res = ft_strdup(str);
+	str = "hello wor ld!", res = ft_strdup(str);
 	printf("%d, '%s' vs '%s'\n", res != str, res, str);
-}
- */
+} */
--- c07/ex01/ft_range.c
+++ c07/ex01/ft_range.c
@@ -6,7 +6,7 @@
 /*   By: aborst <aborst@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/14 19:33:51 by aborst            #+#    #+#             */
-/*   Updated: 2021/09/17 15:31:20 by aborst           ###   ########.fr       */
+/*   Updated: 2021/09/20 16:53:59 by aborst           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ int	*ft_range(int min, int max)
 
 	if (min >= max)
 		return (0);
-	tab = (int *)malloc(sizeof(*tab) * (max - min));
+	tab = malloc(sizeof(*tab) * (max - min));
 	i = 0;
 	while (min < max)
 	{
@@ -34,25 +34,34 @@ int	*ft_range(int min, int max)
 	return (tab);
 }
 
-int	main(void)
+/* int	main(void)
 {
 	int	*res;
 	int	i;
-/* 	res = ft_range(1, 3);
-	for (i = 0; i < 3; i++)
+	int	min;
+	int	max;
+
+	min = 0; max = 5;
+	res = ft_range(min, max);
+	for (i = 0; i < (max - min); i++)
 		printf("%d,", res[i]);
 	printf("\n");
 
-	res = ft_range(-20, -15);
-	for (i = 0; i < 5; i++)
+	min = -20; max = -15;
+	res = ft_range(min, max);
+	for (i = 0; i < (max - min); i++)
 		printf("%d,", res[i]);
 	printf("\n");
 
-	res = ft_range(100, 101);
-	for (i = 0; i < 1; i++)
+	min = 100; max = 115;
+	res = ft_range(min, max);
+	for (i = 0; i < (max - min); i++)
 		printf("%d,", res[i]);
-	printf("\n"); */
+	printf("\n");
 
-	res = ft_range(1, 3);
-	printf("%" PRIxPTR "\n", (uintptr_t) res);
-}
+	min = 100; max = 90;
+	res = ft_range(min, max);
+	for (i = 0; i < (max - min); i++)
+		printf("%d,", res[i]);
+	printf("\n");
+} */
