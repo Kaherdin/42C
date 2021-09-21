9ae1d907efa8898bf4c8d1c09b282b21e577e985
Aurelien Borst <aborst@student.42lausanne.ch>
2021-09-21 15:29:20 +0200

    C08/ex00-01

--- c08/ex01/ft_boolean.h
+++ c08/ex01/ft_boolean.h
@@ -1,7 +1,10 @@
 #ifndef FT_BOOLEAN_H
 # define FT_BOOLEAN_H
 # include <unistd.h>
-# define EVEN(x) x % 2 == 0
+# ifndef EVEN
+#  define EVEN(nbr) (nbr % 2 == 0)
+# endif
+# define VARI  "X a une valeur de %d et y est une string \n",
 # define EVEN_MSG "I have a pair number of arguments."
 # define ODD_MSG "I have an impair number of arguments."
 # define TRUE 1
@@ -9,3 +12,5 @@
 # define SUCCESS 0
 typedef int		t_bool;
 #endif
+
+
--- c08/ex01/main_bool.c
+++ c08/ex01/main_bool.c
@@ -1,22 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft.h                                               :+:      :+:    :+:   */
+/*   main_bool.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aborst <aborst@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/09/16 02:26:13 by aborst            #+#    #+#             */
-/*   Updated: 2021/09/21 14:59:26 by aborst           ###   ########.fr       */
+/*   Created: 2021/09/21 15:17:47 by aborst            #+#    #+#             */
+/*   Updated: 2021/09/21 15:27:21 by aborst           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_H
-# define FT_H
+#include "ft_boolean.h"
 
-void	ft_putchar(char c);
-void	ft_putstr(char *str);
-int		ft_strlen(char *str);
-void	ft_swap(int *a, int *b);
-int		ft_strcmp(char *s1, char s2);
+void	ft_putstr(char *str)
+{
+	while (*str)
+		write(1, str++, 1);
+}
 
-#endif
+t_bool	ft_is_even(int nbr)
+{
+	return ((EVEN(nbr)) ? TRUE : FALSE);
+}
+
+int	main(int argc, char **argv)
+{
+	(void)argv;
+	if (ft_is_even(argc - 1) == TRUE)
+		ft_putstr(EVEN_MSG);
+	else
+		ft_putstr(ODD_MSG);
+	return (SUCCESS);
+}
