a2f1d403efff95a44c907e835291ec1ad4bcf1cf
Aurelien Borst <aborst@student.42lausanne.ch>
2021-09-21 21:05:17 +0200

    C09/ex04 : cc... to validate

---
+++ c08/ex04/ft_stock_str.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_stock_str.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aborst <aborst@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/08/11 18:02:19 by ecaceres          #+#    #+#             */
+/*   Updated: 2021/09/21 21:02:41 by aborst           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_STOCK_STR_H
+# define FT_STOCK_STR_H
+
+typedef struct	s_stock_str
+{
+	int			size;
+	char		*str;
+	char		*copy;
+}				t_stock_str;
+
+#endif
