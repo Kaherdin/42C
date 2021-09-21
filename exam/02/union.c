0174a6a14cf13c979686b28d89e988d06c16b5ef
Aurelien Borst <aborst@student.42lausanne.ch>
2021-09-21 05:08:50 +0200

    mroe example

--- exam/02/union.c
+++ exam/02/union.c
@@ -1,22 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_union.c                                         :+:      :+:    :+:   */
+/*   union.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aborst <aborst@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/09/17 01:27:56 by aborst            #+#    #+#             */
-/*   Updated: 2021/09/21 05:06:50 by aborst           ###   ########.fr       */
+/*   Created: 2021/09/21 04:42:33 by aborst            #+#    #+#             */
+/*   Updated: 2021/09/21 05:08:50 by aborst           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include <unistd.h>
-#include <stdio.h>
-
-void	ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
 
 int	ft_strlen(char *str)
 {
@@ -30,20 +22,6 @@ int	ft_strlen(char *str)
 	return (i);
 }
 
-int	ft_verif_char(char *str, char c, int size)
-{
-	int	i;
-
-	i = 0;
-	while (i < size)
-	{
-		if (str[i] == c)
-			return (0);
-		i++;
-	}
-	return (1);
-}
-
 void	ft_union(char *s1, char *s2)
 {
 	int	i;
@@ -52,24 +30,15 @@ void	ft_union(char *s1, char *s2)
 	i = 0;
 	while (s1[i])
 	{
-		if (ft_verif_char(s1, s1[i], i) == 1)
-			ft_putchar(s1[i]);
-		i++;
-	}
-	j = 0;
-	while (s2[j])
-	{
-		if (ft_verif_char(s2, s2[j], j) == 1)
+		j = 0;
+		while (s2[i])
 		{
-			if (ft_verif_char(s1, s2[j], i) == 1)
-				ft_putchar(s2[j]);
-		}			
-		j++;
+			ft_check_c()
+		}
 	}
 }
 
 int	main(void)
 {
 	ft_union("zpadinton", "paqefwtdjetyiytjneytjoeyjnejeyj");
-	ft_putchar('\n');
 }
